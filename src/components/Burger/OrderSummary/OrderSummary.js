a00d761754fcd1cb85febb2c293430f317f913df
asraavisena <asra.avisena@yahoo.com>
2020-09-19 19:12:16 +0200

    adding price on ordersummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
+
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
             .map(igKey => {
@@ -15,6 +16,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to check out?</p>
             <Button btnType= "Danger" clicked= {props.purchaseCancelled}>CANCEL</Button>
             <Button btnType= "Success" clicked= {props.purchaseContinued}>COUNTINUE</Button>
